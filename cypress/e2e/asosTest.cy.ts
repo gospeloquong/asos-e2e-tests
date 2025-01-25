4af1e8ae694313fa4584df6cd50b261a121d8139
gospeloquong <gospeloquong@gmail.com>
2025-01-25 19:02:32 +0000

    Initial commit

---
+++ cypress/e2e/asosTest.cy.ts
@@ -0,0 +1,8 @@
+describe('ASOS E2E Test', () => {
+    it('adding trainers to bag ', () => {
+        // Visit ASOS homepage
+        cy.visit('https://www.asos.com/');
+        
+        
+    });
+});
